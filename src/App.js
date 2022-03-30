7d1273cf34697cfb76dd526a5cc43e2524b53e81
Charlotte Kindt <37578645+Chachalotte@users.noreply.github.com>
2022-03-31 00:43:28 +0200

    added header, background image header and title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Bienvenue sur le site de Charlotte Kindt.
-        </p>
-      </header>
+// import { Home } from '../src/Pages/Homes/home';
+import ReactDOM from 'react-dom';
+import headerBackground from '../src/Assets/header_background.jpg'
+  function Home(props) {
+    return (
+      
+    <div className="App-header" style={{ backgroundImage: `url(${headerBackground})` }}>
+        <h1 class=" text-3xl font-bold underline">WELCOME</h1>
+        <p class="title">I'M A {props.title}</p>
+        <img src={headerBackground} alt="headerBackground" />
     </div>
+    
+    
+    ) 
+  };
+  
+  function propsTitle() {
+    return (
+      <div>
+        <Home title="developer" />
+        <Home title="graphist" />
+      </div>
+    );
+  }
+  
+  ReactDOM.render(
+    <propsTitle />,
+    document.getElementById('root')
   );
-}
 
-export default App;
+  export default propsTitle;
